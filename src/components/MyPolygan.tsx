f0628c3b0557057581222e08b00eaeb3ccc26434
Marzieh Moghtaderi <marzieh.moghtaderi@deliveryhero.com>
2022-09-25 11:29:48 +0200

    fix(polygan): improve polygan

--- src/components/MyPolygan.tsx
+++ src/components/MyPolygan.tsx
@@ -1,61 +1,72 @@
-import { useEffect } from "react";
+import React from "react";
 
-// import { Debug } from 'astro/components';
-const answer = {
-  y1: "number",
-  y2: "number",
-  theX: "number"
-}
-// input: cx, cy, r
-// output: the arrays of points: [[x1,y1], [x2,y2], ...]
-function calculateThePointsData(cx:number, cy:number, r:number, numberOfPoints:number) {
-  // cx-r=<h=<cx+r => 50<h<150
-  // d = (end-begin)/n = 2r/n => 150-50/4 = 25 => h = 125, 150
-  // (x-cx)^2+(y-cy)^2=r^2 => x^2 + cx^2 -2cx*x + y^2 + cy^2 - 2cy*y - r^2 = 0
-  // moadele daraje 2 e => a = 1, b = -2cy, c= x^2 + cx^2 -2cx*x - r^2 +cy^2
-  // delta = b^2-4ac & y = (-b+/-sqr(delta))/(2a)
-  let points = [];
-  let lastX = cx;
-  const distanceBetweenX = (2 * r) / numberOfPoints;
-  for (; lastX + distanceBetweenX <= cx + r; ) {
-    lastX += distanceBetweenX;
-    points.push(lastX);
+function calculateThePointsData(r, minNumberOfPoints) {
+  let positiveX = [];
+  let standardMinNumberOfPoints =
+    minNumberOfPoints <= 2 ? 2 : Math.ceil(minNumberOfPoints / 2);
+  const distanceBetweenX = (2 * r) / standardMinNumberOfPoints;
+  let lastX = r + distanceBetweenX;
+  for (; lastX - distanceBetweenX >= 0; ) {
+    lastX -= distanceBetweenX;
+    positiveX.push(lastX);
   }
-  points.map((theX) => {
-    let a = 1;
-    let c = (theX ^ 2) + (cx ^ 2) - 2 * cx * theX - (r ^ 2) + (cy ^ 2);
-    let b = cy * -2;
-    let delta = (b ^ 2) - 4 * a * c;
-    let y1 = (b * -1 + Math.sqrt(delta)) / (2 * a);
-    let y2 = (b * -1 - Math.sqrt(delta)) / (2 * a);
-    console.log(y1, y2, theX);
-    return {y1, y2, theX};
-  });
-
-  return { y1: 0, y2:0, theX:0 };
-
+  const polyganPointsHelper = {
+    coordinateArea1: [],
+    coordinateArea2: [],
+    coordinateArea3: [],
+    coordinateArea4: []
+  };
+  const hashMapX = positiveX.reduce((theHashMap, theX) => {
+    let theY = Math.round(Math.sqrt(Math.pow(r, 2) - Math.pow(theX, 2)));
+    polyganPointsHelper.coordinateArea1.push(`${theX},${theY}`);
+    polyganPointsHelper.coordinateArea2.unshift(`${-theX},${theY}`);
+    polyganPointsHelper.coordinateArea3.push(`${-theX},${-theY}`);
+    polyganPointsHelper.coordinateArea4.unshift(`${theX},${-theY}`);
+    return theHashMap.set(theX, theY);
+  }, new Map());
+  const polyganPoints = new Set([
+    ...polyganPointsHelper.coordinateArea1,
+    ...polyganPointsHelper.coordinateArea2,
+    ...polyganPointsHelper.coordinateArea3,
+    ...polyganPointsHelper.coordinateArea4
+  ]);
+  return [positiveX, hashMapX, [...polyganPoints]];
 }
 
 export default function MyPolygan() {
   const BoxSize = 200;
   const sideSize = BoxSize / 2;
-  const r = BoxSize / 4;
-
-  // const numberOfPoints =
-  // calculateThePointsData(0, 0, r, 4);
-  // <Debug answer={calculateThePointsData(0, 0, r, 4)} />
+  const r = (BoxSize - 10) / 2;
+  const [positiveX, hashMapX, polyganPoints] = calculateThePointsData(r, 10);
 
   return (
-		<>
-			<svg
-				viewBox={`${sideSize * -1} ${sideSize * -1} ${BoxSize} ${BoxSize}`}
-				height={BoxSize}
-				width={BoxSize}
-				fill="transparent"
-			>
-				<circle cx="0" cy="0" r={r} stroke="black" />
-				<circle cx="0" cy="0" r="2" stroke="red" />
-			</svg>
-		</>
+    <>
+      <svg
+        viewBox={`${-sideSize} ${-sideSize} ${BoxSize} ${BoxSize}`}
+        fill="transparent"
+      >
+        {positiveX &&
+          positiveX.map((x) => {
+            const y = hashMapX.get(x);
+
+            return (
+              <>
+                <circle cx={x} cy={y} r="1" stroke="red" />
+                <circle cx={x} cy={-y} r="1" stroke="red" />
+                <circle cx={-x} cy={y} r="1" stroke="red" />
+                <circle cx={-x} cy={-y} r="1" stroke="red" />
+              </>
+            );
+          })}
+        {positiveX &&
+          positiveX.map((x) => {
+            return (
+              <>
+                <polygon points={polyganPoints} stroke="Orange" />
+              </>
+            );
+          })}
+      </svg>
+    </>
   );
 }
